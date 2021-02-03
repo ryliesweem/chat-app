2bb72284f3ef2fd3e772ac435b16836d9c479895
Rylie Sweem <ryliesweem@gmail.com>
2021-02-03 12:05:32 -0800

    name picker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import TextInput from './TextInput';
 import Message from './Message';
+import NamePicker from './NamePicker';
 
 function App() {
   const [messages,setMessages] = useState([])
@@ -9,6 +10,7 @@ function App() {
 
     <header className="header">
       <i class="far fa-comments logo"></i><h1>Chatter</h1>
+      <NamePicker />
     </header>
 
     <div className="messages">
---
+++ src/NamePicker.js
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+
+function NamePicker(){
+    const [showInput, setShowInput] = useState(false)
+    const [username, setUsername] = useState('')
+
+    if (showInput) {
+        return <div className="name-picker">
+            <input value={username}
+                onChange={e=> setUsername(e.target.value)}
+            />
+            <button onClick={()=>setShowInput(false)}>
+                <i class="fas fa-check"></i>
+            </button>
+        </div>
+    }
+
+    return <div className="name-picker">
+        <div className="name">{username}</div>
+        <button onClick={()=>setShowInput(true)}>
+            <i class="fas fa-edit"></i>
+        </button>
+    </div>
+}
+
+export default NamePicker
